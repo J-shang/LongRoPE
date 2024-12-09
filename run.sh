61de9225027564364658efd6b719a0a3ffc702e4
nishang <nishang@microsoft.com>
2024-12-09 07:59:15 +0000

    update

---
+++ run.sh
@@ -0,0 +1,4 @@
+# CRITICAL_DIM=27 bash /scratch/nishang/LongRoPE/search-phi4-long-factor.sh
+# CRITICAL_DIM=31 bash /scratch/nishang/LongRoPE/search-phi4-long-factor.sh
+# CRITICAL_DIM=29 bash /scratch/nishang/LongRoPE/search-phi4-long-factor.sh
+CRITICAL_DIM=34 bash /scratch/nishang/LongRoPE/search-phi4-long-factor.sh
--- search-phi3-long-factor.sh
+++ search-phi3-long-factor.sh
@@ -1,11 +1,11 @@
-MODEL_PATH=/scratch/nishang/models/phi_omni-218000-mistral-newtokenizer-no-flash
-DATASET_PATH=/scratch/nishang/datasets/pg19-train-128k-search-phi-omni-new-tokenized-hf
+MODEL_PATH=/scratch/nishang/models/mistral_version
+DATASET_PATH=/scratch/nishang/datasets/pg19-train-8k-search-phi31-tokenized-hf
 
-TARGET_LENGTH=131072
-LENGTH_SCALE=64
-# CRITICAL_DIM=25
+TARGET_LENGTH=8192
+LENGTH_SCALE=8
+CRITICAL_DIM=25
 
-RESULT_PATH=/scratch/nishang/longrope-search/phi4-omni-mscale/critical_dim_$CRITICAL_DIM/
+RESULT_PATH=/scratch/nishang/longrope-search/phi31-mscale/critical_dim_$CRITICAL_DIM/
 
 mkdir -p $RESULT_PATH
 
@@ -24,4 +24,4 @@ python evolution/search.py \
     --length-scale $LENGTH_SCALE \
     --num-proc 16 \
     --save-memory \
-    --critical-dim $CRITICAL_DIM > log_phi4_omni_cd_$CRITICAL_DIM.txt 2>&1
+    --critical-dim $CRITICAL_DIM > log_phi31_cd_$CRITICAL_DIM.txt 2>&1
